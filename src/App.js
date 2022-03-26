af9322eccf9242667609c3d883fd71101fff4062
SuparnaAishee <suparnad806Qgmail.com>
2022-03-26 22:36:31 +0600

    5th

--- src/App.js
+++ src/App.js
@@ -1,2 +1,19 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Items from "./Component/Items/Items";
 
+function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("items.json")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
 
+  return (
+    <div>
+      <Items data={data}> </Items>
+    </div>
+  );
+}
+export default App;
